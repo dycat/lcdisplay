0823928adb18843ccbbf267a733191070e9c988f
dycat <dyangem@hotmail.com>
2022-04-23 23:06:19 +0800

    use pipenv

--- src/time.py
+++ src/time.py
@@ -7,8 +7,10 @@ lcd = LCD()
 # lcd.BACKGROUND_LIGHT = 0
 while True:
     now = datetime.now()
-    if now.hour > 22 or now.hour < 8:
+    if now.hour >= 22 or now.hour < 8:
         lcd.BACKGROUND_LIGHT = 0
         # lcd.turn_light()
+    else:
+        lcd.BACKGROUND_LIGHT = 1
     now_string = now.strftime("%b %d %H:%M")
     lcd.print_lcd(0, 0, now_string)
